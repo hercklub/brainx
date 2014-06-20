7e2d03e7cd600854c7ee0d7af8728b352ce3268b
Ľuboš Herčka <hercklub@gmail.com>
2014-06-20 00:26:43 +0000

    Added basic Brainfuck funcionality

--- brainx.py
+++ brainx.py
@@ -1,6 +1,6 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import sys
 
 class BrainFuck:
     """Interpretr jazyka brainfuck."""
@@ -8,17 +8,83 @@ class BrainFuck:
     def __init__(self, data, memory=b'\x00', memory_pointer=0):
         """Inicializace interpretru brainfucku."""
         
-        # data programu
-        self.data = data
-        
         # inicializace proměnných
-        self.memory = memory
+        self.memory = bytearray(memory)
         self.memory_pointer = memory_pointer
+
+        try:
+            with open(data, mode='r') as f:
+                self.code = f.read()
+        except:
+            self.code = data
         
         # DEBUG a testy
         # a) paměť výstupu
         self.output = ''
-    
+        if self.code.count('[')!=self.code.count(']'):
+            print ("Bad fromat")
+            sys.exit(1)
+        
+
+        #self.input=self.find_input()
+        self.interpreter(self.code)
+        
+
+    def interpreter(self,code):
+        """ Interprets brainfuck code"""
+        code_ptr=0
+        while code_ptr<len(code):
+            command=code[code_ptr]
+                
+            if command==">":
+                self.memory_pointer += 1
+
+                if self.memory_pointer==len(self.memory):
+                    self.memory+=bytearray([0])
+
+            if command=="<":
+                self.memory_pointer -= 1
+
+                if self.memory_pointer<0:
+                    self.memory_pointer=0
+
+            if command=="+":
+                self.memory[self.memory_pointer]+=1
+
+                if self.memory[self.memory_pointer] > 255:
+                    self.memory[self.memory_pointer]=0
+
+            if command=="-":
+                self.memory[self.memory_pointer]-=1
+                if self.memory[self.memory_pointer]<0:
+                    self.memory[self.memory_pointer]=255
+
+            if command == ".":
+                sys.stdout.write(chr(self.memory[self.memory_pointer]))
+                towrite=chr(self.memory[self.memory_pointer])
+                self.output+=towrite
+
+            if command==",":
+                self.memory[self.memory_pointer]=ord(self.read_char())
+
+            if command=="[":
+                loopcode = self.get_loopcode(code[code_ptr:])
+                while self.memory[self.memory_pointer] != 0:
+                    self.interpreter(loopcode)
+                code_ptr += len(loopcode) + 1
+            
+            code_ptr += 1               
+
+    def get_loopcode(self,code):
+        
+        end = 1
+        while (code[0:end].count('[') != code[0:end].count(']')):
+            end += 1
+        
+       
+        return code[1:end-1]
+
+        
     #
     # pro potřeby testů
     #
@@ -51,3 +117,5 @@ class BrainCopter():
         self.program = BrainFuck(self.data)
 
 
+if __name__ == '__main__':
+     BrainFuck(sys.argv[1])
